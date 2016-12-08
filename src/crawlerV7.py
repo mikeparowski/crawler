10a2f97249e52d10c1488faa359a7b4742fa11e0
Michael <michael.parowski@gmail.com>
2016-12-08 18:47:01 -0500

    almost done

--- src/crawlerV7.py
+++ src/crawlerV7.py
@@ -4,7 +4,9 @@ import requests
 import re
 
 # Global Variables
-fulllink = re.compile("^https://")
+https_www = re.compile("^https://")
+http = re.compile("^http://")
+www = re.compile("^www.")
 sublink = re.compile("^/")
 allelse = re.compile("#.*")
 ndDomain = re.compile(".*nd\.edu.*")
@@ -13,8 +15,12 @@ ndDomain = re.compile(".*nd\.edu.*")
 def trade_spider(max_pages):
     page = 1
     with warnings.catch_warnings():
+        url = "https://www.nd.edu"
+        visited = set()
         while page <= max_pages:
-            url = "https://www.nd.edu"
+            i = 0
+            if url in visited:
+                continue 
             try:
                 source_code = requests.get(url)
             except:
@@ -24,29 +30,48 @@ def trade_spider(max_pages):
             for link in soup.find_all('a', attrs={'href': re.compile(".*")}):
                 if sublink.match(link.get('href')):
                     href = url + link.get('href')
-                elif fulllink.match(link.get('href')):
+                elif https_www.match(link.get('href')):
                     href = link.get('href')
                 else:
                     continue
                 if not ndDomain.match(href):
                     continue
+                if link.get('href').endswith('.php') or link.get('href').endswith('.pdf'):
+                    continue
                 
                 title = link.string
-                links = get_single_item_data(href)
-                print href
-                for l in links:
-                    print l[0],
+                links = crawl(href)
+                unique_links = set(links)
+                length = len(links)
+                print href[12:-1] # slice out https://www. and final /
+                for l in unique_links:
+                    if l[1] is "None" or l[1] is None: # print abbreviated url if no title 
+                        abbrv = l[0]
+                        abbrv.encode('utf-8').decode('utf-8')
+                        if https_www.match(abbrv):
+                            print abbrv[12:-1], # https://www.
+                        elif http.match(abbrv):
+                            print abbrv[7:-1], # http://
+                        elif www.match(abbrv):
+                            print abbrv[4:-1], # www.
+                    else:
+                        t = l[1]
+                        t.encode('utf-8').decode('utf-8')
+                        print t, # print title 
                 print '\n'
+            visited.add(url)
+            if i < length:
+                url = links[i]
+                i += 1
             page += 1
 
-def get_single_item_data(item_url):
+def crawl(item_url):
     links = []
     
     source_code = requests.get(item_url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
-#    for item_name in soup.find_all('nav', {'class': 'navigation'}):
-#        print(item_name.string)
+
     for link in soup.find_all('a', attrs={'href': re.compile(".*")}):
         if sublink.match(link.get('href')):
             href = "https://nd.edu" + link.get('href')
@@ -56,6 +81,9 @@ def get_single_item_data(item_url):
             href = link.get('href')
         if not ndDomain.match(href):
             continue
+        if link.get('href').endswith('.php') or link.get('href').endswith('.pdf'):
+            continue
+
         title = link.string
         links.append((href, title))
 
